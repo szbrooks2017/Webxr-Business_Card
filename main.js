311fb76c62dd769feddf25c5028dc98fef4207b8
Stratton Brooks <69229545+szbrooks2017@users.noreply.github.com>
2022-02-23 15:55:02 -0600

    start the webgl

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ var button = document.getElementById("webgl");
 
 button.onclick = function(){
     console.log("opened");
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    const gl = canvas.getContext("webgl", {xrCompatible: true});
 };
